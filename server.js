d320417f661042c1728fcab04f51bb5e3ffaf9d1
Rome Bell <romebell5@gmail.com>
2020-11-24 06:26:26 -0800

    [server] Remove logging of SECRET_SESSION

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const session = require('express-session');
 const passport = require('./config/ppConfig');
 const flash = require('connect-flash');
 const SECRET_SESSION = process.env.SECRET_SESSION;
-console.log(SECRET_SESSION);
 const app = express();
 
 // isLoggedIn middleware
